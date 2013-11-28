26f228321f0f0a884f334a365fc6e64e4dc17a7c
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-27 17:43:33 -0800

    Passed first test

--- test/spec/LibraryEntryViewSpec.js
+++ test/spec/LibraryEntryViewSpec.js
@@ -5,9 +5,9 @@ describe('LibraryEntryView', function() {
     model = new MyTunes.Models.SongModel({
       artist: 'Fakey McFakerson',
       title: 'Never Gonna Mock You Up',
-      url: 'example/url',
+      url: 'example/url'
     });
-    // spyOn(MyTunes.Models.SongModel.prototype, 'enqueue'); // Uncomment this when working on the second test
+    spyOn(MyTunes.Models.SongModel.prototype, 'enqueue'); // Uncomment this when working on the second test
     spyOn(MyTunes.Models.SongModel.prototype, 'play');
     view = new MyTunes.Views.LibraryEntryView({model: model});
     view.render();
@@ -19,7 +19,7 @@ describe('LibraryEntryView', function() {
   });
 
   // Comment out the above spec when implementing the below
-  xit('queues clicked songs', function(){
+  it('queues clicked songs', function(){
     view.$el.children().first().click();
     expect(model.enqueue).toHaveBeenCalled();
   });
